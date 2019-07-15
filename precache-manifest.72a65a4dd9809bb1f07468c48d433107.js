9ad228b04092ec3ca1b6062a1ff2fbdfc725d233
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-07-14 23:18:11 -0300

    Updates

---
+++ precache-manifest.72a65a4dd9809bb1f07468c48d433107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bb02c3f1f26103194f78669721d3be",
+    "url": "/fifteen-puzzle-game/index.html"
+  },
+  {
+    "revision": "bda283fa6b0eb9434755",
+    "url": "/fifteen-puzzle-game/static/css/main.60af98a0.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/fifteen-puzzle-game/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "bda283fa6b0eb9434755",
+    "url": "/fifteen-puzzle-game/static/js/main.017a445b.chunk.js"
+  },
+  {
+    "revision": "b701ca85abd3725094ab",
+    "url": "/fifteen-puzzle-game/static/js/runtime~main.27c4b8cf.js"
+  }
+]);
